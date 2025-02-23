857b09f17a70c9eb87ddedecee5d17c711656fe7
dorapon48 <dorapon48@icloud.com>
2025-02-23 15:21:43 +0900

    update: ディスク範囲を修正

--- iniconfig.hpp
+++ iniconfig.hpp
@@ -3,19 +3,19 @@
 
 #include <string>
 #include <limits>
-#define MAX_DISKS 100
+#include <vector>
 
 struct DiskConfig {
-    std::string section_name = "";
-    std::string mount_path = "";
+    std::string section_name;
+    std::string mount_path;
     int disk_usage_max_limit = INT32_MIN;
 };
 
 struct IniConfig {
-    int disk_usage_max_limit = INT32_MIN;
-    std::string to_mail = "";
-    std::string from_mail = "";
-    DiskConfig disks[MAX_DISKS];
+    int disk_usage_max_limit = 80;
+    std::string to_mail;
+    std::string from_mail;
+    std::vector<DiskConfig> disks;
     int disk_count = 0;
 };
 
