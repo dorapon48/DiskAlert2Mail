c34ec6ea099f081d3b2543bf288ecd0b0ba7c2bb
dorapon48 <dorapon48@icloud.com>
2025-02-24 16:44:45 +0900

    add: test.py

---
+++ main/test.py
@@ -0,0 +1,12 @@
+import tomllib
+import subprocess
+
+with open("d2m.toml", "rb") as file:  # "rb" でバイナリ読み込み
+    config = tomllib.load(file)
+
+print(config)  # 読み込んだ TOML データを表示
+
+result = subprocess.run(["df", "-h"], capture_output=True, text=True)
+lines = result.stdout.split("\n")
+for i in lines:
+    print(i)
