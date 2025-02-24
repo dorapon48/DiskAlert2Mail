9e7beb67954633fc2d3aa8398ab9e1458264fe5a
dorapon48 <dorapon48@icloud.com>
2025-02-24 18:07:21 +0900

    rename: main/test.py -> main/test/test.py update: read_tomlを関数に変更

--- main/read_toml.py
+++ main/read_toml.py
@@ -2,16 +2,20 @@ import tomllib
 from pydantic import ValidationError
 from model import Config
 
-with open("d2m.toml", "rb") as file:  # "rb" でバイナリ読み込み
-    config = tomllib.load(file)
+def read_toml():
+    with open("d2m.toml", "rb") as file:  # "rb" でバイナリ読み込み
+        config = tomllib.load(file)
 
-# print(config)  # 読み込んだ TOML データを表示
+    # print(config)  # 読み込んだ TOML データを表示
 
-try:
-    config = Config.model_validate(config)
-    print(config)
-    print("Valid TOML!")
-except ValidationError as e:
-    print("Invalid TOML:", e)
-except ValueError as e:
-    print("Validation Error:", e)
+    try:
+        config = Config.model_validate(config)
+        # print(config)
+        print("Valid TOML!")
+        return config
+    except ValidationError as e:
+        print("Invalid TOML:", e)
+        return None
+    except ValueError as e:
+        print("Validation Error:", e)
+        return None
