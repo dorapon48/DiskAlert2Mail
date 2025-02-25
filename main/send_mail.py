1e479cf8ff102120cd9beb9ab94919f35d5dd9a2
dorapon48 <dorapon48@icloud.com>
2025-02-25 18:37:08 +0900

    update: send_mail構築

--- main/send_mail.py
+++ main/send_mail.py
@@ -1,2 +1,21 @@
 import smtplib
+from email.mime.text import MIMEText
+from email.utils import formatdate
+from model import MailConfig
 
+def send_mail(config: MailConfig, bodyText: str, subject: str):
+    # メールをMIME形式で作成
+    msg = MIMEText(bodyText)
+    msg['Subject'] = subject
+    msg['From'] = config.fromAddress
+    msg['To'] = config.toAddress
+    msg['Date'] = formatdate()
+
+    # 接続
+    connection = smtplib.SMTP(config.smtpAddress, config.smtpPort)
+    connection.set_debuglevel(True) # enable debug output
+    connection.starttls()
+    connection.login(config.smtpUser, config.smtpPassword)
+    # 送信
+    connection.sendmail(config.fromAddress, config.toAddress, msg.as_string())
+    connection.close()
