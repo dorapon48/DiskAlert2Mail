a39928f38fd0a0034257e154f446a50f2e34aedb
dorapon48 <dorapon48@icloud.com>
2025-02-26 16:45:12 +0900

    add: create_mail_body_text

--- main/main.py
+++ main/main.py
@@ -5,6 +5,30 @@ from read_toml import read_toml
 from get_df import get_df
 
 
+def create_mail_body_text(mail_body_list: List[MailBody]):
+    """
+    メールの本文を作成する
+    """
+    msg = """
+Disk Alert To Mail
+以下のディスクの使用率が設定された値を上回りました．
+
+"""
+    for m in mail_body_list:
+        c = m.config
+        d = m.df
+        if (c.mountPath == ""): mount_p = "/"
+        else : mount_p = c.mountPath
+        re_amount = round((d.available - d.used) / 1024 ** 2, 2)
+        msg += "ディスク名: " + c.diskName + "\n"
+        msg += "マウント先: " + mount_p + "\n"
+        msg += "設定使用率: " + str(c.diskUsageMaxLimit) + "\n"
+        msg += "使用率　　: " + "{:.2f}".format((d.used / d.available) * 100) + "\n"
+        msg += "残り容量　: " + str(re_amount) + "GB" + "\n"
+        msg += "\n"
+    return msg
+        
+
 def check_disk(disk: DiskConfig, df_list: List[DiskUseage]):
     """
     ディスクの判定をする
@@ -13,7 +37,7 @@ def check_disk(disk: DiskConfig, df_list: List[DiskUseage]):
     """
     for index, df in enumerate(df_list):
         if (df.mounted_on == "/mnt" + disk.mountPath):
-            useage = df.used / df.available
+            useage = (df.used / df.available) * 100
             if (useage >= disk.diskUsageMaxLimit):
                 return index
             else:
@@ -30,7 +54,9 @@ if __name__ == "__main__":
     for d in config.disks:
         df_index = check_disk(d, df)
         if df_index != -1:
-            overd_disks.append(MailBody(config, df[df_index]))
+            overd_disks.append(MailBody(config=d, df=df[df_index]))
+    msg = create_mail_body_text(overd_disks)
+    print(msg)
 
     
 
