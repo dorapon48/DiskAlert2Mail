990a30b99b2a33d7c6fdb3a839cfb6049bf7d462
dorapon48 <dorapon48@icloud.com>
2025-02-26 14:57:13 +0900

    add: DiskUseage update: mount_path_slash追加

--- main/model.py
+++ main/model.py
@@ -6,6 +6,13 @@ class DiskConfig(BaseModel):
     mountPath: str
     diskUsageMaxLimit: Optional[int] = Field(None, ge=0, le=100)
 
+    # mountPathが/のみの時, 空文字列を設定する
+    @model_validator(mode="after")
+    def mount_path_slash(self):
+        if self.mountPath == "/":
+            self.mountPath = ""
+        return self
+
 class MailConfig(BaseModel):
     toAddress: str
     fromAddress: str
@@ -32,3 +39,10 @@ class Config(BaseModel):
             if disk.diskUsageMaxLimit is None:  # diskUsageMaxLimit が指定されていない場合
                 disk.diskUsageMaxLimit = self.config.diskUsageMaxLimit  # グローバル値を代入
         return self
+
+class DiskUseage(BaseModel):
+    filesystem: str
+    size: int
+    used: int
+    available: int
+    mounted_on: str
